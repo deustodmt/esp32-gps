600f3eadca0f6791151a03ebfe8f0cca9a244b01
DeustoMotoTeam <deustodmt@deusto.es>
2025-09-22 16:57:22 +0200

    mensage

--- src/main.cpp
+++ src/main.cpp
@@ -9,215 +9,157 @@
   copies or substantial portions of the Software.
 */
 
-// Original code: https://github.com/Xinyuan-LilyGO/LilyGO-T-SIM7000G/blob/master/examples/Arduino_NetworkTest/Arduino_NetworkTest.ino
+// Based on the following example: https://github.com/Xinyuan-LilyGO/LilyGO-T-SIM7000G/blob/master/examples/Arduino_TinyGSM/AllFunctions/AllFunctions.ino
 
 #define TINY_GSM_MODEM_SIM7000
 #define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
-
 #define SerialAT Serial1
-// Set serial for debug console (to the Serial Monitor, default speed 115200)
-#define SerialMon Serial
 
 // See all AT commands, if wanted
-// #define DUMP_AT_COMMANDS
+#define DUMP_AT_COMMANDS
 
-// set GSM PIN, if any
-#define GSM_PIN ""
-
-// Your GPRS credentials, if any
-const char apn[]  = "";     //SET TO YOUR APN
-const char gprsUser[] = "";
-const char gprsPass[] = "";
+// Set phone number, if you want to test SMS
+// Set a recipient phone number to test sending SMS (it must be in international format including the "+" sign)
+#define SMS_TARGET  ""
 
 #include <TinyGsmClient.h>
 #include <SPI.h>
 #include <SD.h>
 #include <Ticker.h>
 
-#define GSM_NL "\r\n"
-
-#ifdef DUMP_AT_COMMANDS
+#ifdef DUMP_AT_COMMANDS  // if enabled it requires the streamDebugger lib
   #include <StreamDebugger.h>
-  StreamDebugger debugger(SerialAT, SerialMon);
+  StreamDebugger debugger(SerialAT, Serial);
   TinyGsm modem(debugger);
 #else
   TinyGsm modem(SerialAT);
 #endif
 
-// LilyGO T-SIM7000G Pinout
-#define UART_BAUD           9600
-#define PIN_DTR             25
-#define PIN_TX              27
-#define PIN_RX              26
-#define PWR_PIN             4
+#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP  60          // Time ESP32 will go to sleep (in seconds)
 
-#define SD_MISO             2
-#define SD_MOSI             15
-#define SD_SCLK             14
-#define SD_CS               13
-#define LED_PIN             12
+#define UART_BAUD   9600
+#define PIN_DTR     25
+#define PIN_TX      27
+#define PIN_RX      26
+#define PWR_PIN     4
 
+#define SD_MISO     2
+#define SD_MOSI     15
+#define SD_SCLK     14
+#define SD_CS       13
+#define LED_PIN     12
 
-void modemPowerOn(){
-  pinMode(PWR_PIN, OUTPUT);
-  digitalWrite(PWR_PIN, LOW);
-  delay(1000);
-  digitalWrite(PWR_PIN, HIGH);
-}
-
-void modemPowerOff(){
-  pinMode(PWR_PIN, OUTPUT);
-  digitalWrite(PWR_PIN, LOW);
-  delay(1500);
-  digitalWrite(PWR_PIN, HIGH);
-}
-
-
-void modemRestart(){
-  modemPowerOff();
-  delay(1000);
-  modemPowerOn();
-}
+int counter, lastIndex, numberOfPieces = 24;
+String pieces[24], input;
 
 void setup(){
   // Set console baud rate
-  SerialMon.begin(115200);
-
+  Serial.begin(9600);
   delay(10);
 
   // Set LED OFF
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
 
-  modemPowerOn();
+  pinMode(PWR_PIN, OUTPUT);
+  digitalWrite(PWR_PIN, HIGH);
+  delay(300);
+  digitalWrite(PWR_PIN, LOW);
 
-  Serial.println("========SDCard Detect.======");
-  SPI.begin(SD_SCLK, SD_MISO, SD_MOSI);
+  SPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
   if (!SD.begin(SD_CS)) {
-      Serial.println("SDCard MOUNT FAIL");
+    Serial.println("SDCard MOUNT FAIL");
   } else {
     uint32_t cardSize = SD.cardSize() / (1024 * 1024);
     String str = "SDCard Size: " + String(cardSize) + "MB";
     Serial.println(str);
   }
-  Serial.println("===========================");
+
+  Serial.println("\nWait...");
+
+  delay(1000);
 
   SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
 
-
-  Serial.println("/**********************************************************/");
-  Serial.println("To initialize the network test, please make sure your LTE ");
-  Serial.println("antenna has been connected to the SIM interface on the board.");
-  Serial.println("/**********************************************************/\n\n");
-
-  delay(10000);
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  Serial.println("Initializing modem...");
+  if (!modem.restart()) {
+    Serial.println("Failed to restart modem, attempting to continue without restarting");
+  }
 }
 
 void loop(){
-  String res;
-
-  Serial.println("========INIT========");
-
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  Serial.println("Initializing modem...");
   if (!modem.init()) {
-    modemRestart();
-    delay(2000);
     Serial.println("Failed to restart modem, attempting to continue without restarting");
-    return;
   }
-
-  Serial.println("========SIMCOMATI======");
-  modem.sendAT("+SIMCOMATI");
-  modem.waitResponse(1000L, res);
-  res.replace(GSM_NL "OK" GSM_NL, "");
-  Serial.println(res);
-  res = "";
-  Serial.println("=======================");
-
-  Serial.println("=====Preferred mode selection=====");
-  modem.sendAT("+CNMP?");
-  if (modem.waitResponse(1000L, res) == 1) {
-    res.replace(GSM_NL "OK" GSM_NL, "");
-    Serial.println(res);
-  }
-  res = "";
-  Serial.println("=======================");
-
-
-  Serial.println("=====Preferred selection between CAT-M and NB-IoT=====");
-  modem.sendAT("+CMNB?");
-  if (modem.waitResponse(1000L, res) == 1) {
-    res.replace(GSM_NL "OK" GSM_NL, "");
-    Serial.println(res);
-  }
-  res = "";
-  Serial.println("=======================");
-
 
   String name = modem.getModemName();
+  delay(500);
   Serial.println("Modem Name: " + name);
 
   String modemInfo = modem.getModemInfo();
+  delay(500);
   Serial.println("Modem Info: " + modemInfo);
+  
 
-  // Unlock your SIM card with a PIN if needed
-  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
-    modem.simUnlock(GSM_PIN);
+  // --------TESTING GPS--------
+  
+  Serial.println("\n---Starting GPS TEST---\n");
+  // Set SIM7000G GPIO4 HIGH ,turn on GPS power
+  // CMD:AT+SGPIO=0,4,1,1
+  // Only in version 20200415 is there a function to control GPS power
+  modem.sendAT("+SGPIO=0,4,1,1");
+  if (modem.waitResponse(10000L) != 1) {
+    DBG(" SGPIO=0,4,1,1 false ");
   }
-
-  for (int i = 0; i <= 4; i++) {
-    uint8_t network[] = {
-        2,  /*Automatic*/
-        13, /*GSM only*/
-        38, /*LTE only*/
-        51  /*GSM and LTE only*/
-    };
-    Serial.printf("Try %d method\n", network[i]);
-    modem.setNetworkMode(network[i]);
-    delay(3000);
-    bool isConnected = false;
-    int tryCount = 60;
-    while (tryCount--) {
-      int16_t signal =  modem.getSignalQuality();
-      Serial.print("Signal: ");
-      Serial.print(signal);
-      Serial.print(" ");
-      Serial.print("isNetworkConnected: ");
-      isConnected = modem.isNetworkConnected();
-      Serial.println( isConnected ? "CONNECT" : "NO CONNECT");
-      if (isConnected) {
-        break;
-      }
-      delay(1000);
-      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    }
-    if (isConnected) {
-        break;
-    }
-  }
-  digitalWrite(LED_PIN, HIGH);
-
-  Serial.println();
-  Serial.println("Device is connected .");
-  Serial.println();
-
-  Serial.println("=====Inquiring UE system information=====");
-  modem.sendAT("+CPSI?");
-  if (modem.waitResponse(1000L, res) == 1) {
-    res.replace(GSM_NL "OK" GSM_NL, "");
-    Serial.println(res);
-  }
-
-  Serial.println("/**********************************************************/");
-  Serial.println("After the network test is complete, please enter the  ");
-  Serial.println("AT command in the serial terminal.");
-  Serial.println("/**********************************************************/\n\n");
-
+  modem.enableGPS();
+  float lat,  lon;
   while (1) {
-    while (SerialAT.available()) {
-      SerialMon.write(SerialAT.read());
+    if (modem.getGPS(&lat, &lon)) {
+      Serial.printf("lat:%f lon:%f\n", lat, lon);
+      break;
+    } else {
+      Serial.print("getGPS ");
+      Serial.println(millis());
     }
-    while (SerialMon.available()) {
-      SerialAT.write(SerialMon.read());
-    }
+    delay(2000);
+  }
+  modem.disableGPS();
+
+  // Set SIM7000G GPIO4 LOW ,turn off GPS power
+  // CMD:AT+SGPIO=0,4,1,0
+  // Only in version 20200415 is there a function to control GPS power
+  modem.sendAT("+SGPIO=0,4,1,0");
+  if (modem.waitResponse(10000L) != 1) {
+    DBG(" SGPIO=0,4,1,0 false ");
+  }
+  Serial.println("\n---End of GPRS TEST---\n");
+
+
+  // --------TESTING POWER DONW--------
+
+  // Try to power-off (modem may decide to restart automatically)
+  // To turn off modem completely, please use Reset/Enable pins
+  modem.sendAT("+CPOWD=1");
+  if (modem.waitResponse(10000L) != 1) {
+    DBG("+CPOWD=1");
+  }
+  // The following command does the same as the previous lines
+  modem.poweroff();
+  Serial.println("Poweroff.");
+
+  // GO TO SLEEP
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  delay(200);
+  esp_deep_sleep_start();
+
+  // Do nothing forevermore
+  while (true) {
+      modem.maintain();
   }
 }
